90bb90dd8cdb49274a896f87ae2a8a4d4726415a
Whirlwindraven <fadysherif1998@gmail.com>
2022-12-07 15:27:53 -0500

    Added questions to the question.js file

--- assets/questions.js
+++ assets/questions.js
@@ -1,44 +1,58 @@
-{
-    "Questions": [
-        {
-            "Test": "Q.1) What is Javascript used for in web development?",
-            "option1": "Styling",
-            "option2": "Creating elements",
-            "option3": "Logic and interactivity",
-            "option4": "none of the above",
-            "answer": "Logic and interactivity"
-        },
-        {
-            "Test": "Q.2) How can a datatype be declared to be a constant type?",
-            "option1": "Const",
-            "option2": "Var",
-            "option3": "let",
-            "option4": "constant",
-            "answer": "Const"
-        },
-        {
-            "Test": "Q.3) What keyword is used to check whether a given property is valid?",
-            "option1": "in",
-            "option2": "is in",
-            "option3": "exists",
-            "option4": "lies",
-            "answer": "in"
-        },
-        {
-            "Test": "Q.4) Javascript is an ________ language?",
-            "option1": "Object-oriented",
-            "option2": "Object-based",
-            "option3": "Procedural",
-            "option4": "None of the above",
-            "answer": "Object-oriented"
-        },
-        {
-            "Test": "Q.5) Upon encountering empty statements, whar does the Javascript Interpreter do?",
-            "option1": "Throws an error",
-            "option2": "Ignores the statements",
-            "option3": "Gives a warning", 
-            "option4": "None of the above",
-            "answer": "Ignores the statements"
-        }
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is Javascript used for in web development?",
+    answer: "Logic and interactivity",
+    options: [
+        "Styling",
+        "Creating elements",
+        "Logic and interactivity",
+        "none of the above",
     ]
-}
+  },
+    {
+    numb: 2,
+    question: "How can a datatype be declared to be a constant type?",
+    answer: "Const",
+    options: [
+        "Const",
+        "Var",
+        "let",
+        "constant",
+    ]
+  },
+    {
+    numb: 3,
+    question: "What keyword is used to check whether a given property is valid?",
+    answer: "in",
+    options: [
+         "in",
+         "is in",
+         "exists",
+         "lies",
+    ]
+  },
+    {
+    numb: 4,
+    question: "Javascript is an ________ language?",
+    answer: "Object-oriented",
+    options: [
+         "Object-oriented",
+         "Object-based",
+         "Procedural",
+         "None of the above",
+    ]
+  },
+    {
+    numb: 5,
+    question: "Upon encountering empty statements, whar does the Javascript Interpreter do?",
+    answer: "Ignores the statements",
+    options: [
+        "Throws an error",
+        "Ignores the statements",
+        "Gives a warning", 
+        "None of the above",
+    ]
+  },
+];
