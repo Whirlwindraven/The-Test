ca8dcc25c2175cf63a4813d30f90458e95081322
Whirlwindraven <fadysherif1998@gmail.com>
2022-12-07 12:09:14 -0500

    deleted json file and added questions array to script

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+{
+    "Questions": [
+        {
+            "Test": "Q.1) What is Javascript used for in web development?",
+            "option1": "Styling",
+            "option2": "Creating elements",
+            "option3": "Logic and interactivity",
+            "option4": "none of the above",
+            "answer": "Logic and interactivity"
+        },
+        {
+            "Test": "Q.2) How can a datatype be declared to be a constant type?",
+            "option1": "Const",
+            "option2": "Var",
+            "option3": "let",
+            "option4": "constant",
+            "answer": "Const"
+        },
+        {
+            "Test": "Q.3) What keyword is used to check whether a given property is valid?",
+            "option1": "in",
+            "option2": "is in",
+            "option3": "exists",
+            "option4": "lies",
+            "answer": "in"
+        },
+        {
+            "Test": "Q.4) Javascript is an ________ language?",
+            "option1": "Object-oriented",
+            "option2": "Object-based",
+            "option3": "Procedural",
+            "option4": "None of the above",
+            "answer": "Object-oriented"
+        },
+        {
+            "Test": "Q.5) Upon encountering empty statements, whar does the Javascript Interpreter do?",
+            "option1": "Throws an error",
+            "option2": "Ignores the statements",
+            "option3": "Gives a warning", 
+            "option4": "None of the above",
+            "answer": "Ignores the statements"
+        }
+    ]
+}
